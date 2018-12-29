19dc54b6c2cbcd55d63f8ec8f599822d44027114
Avinash <avinashavi1245@gmail.com>
2018-12-29 10:22:43 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+django-jquery-file-upload
+"""
+
+from setuptools import setup, find_packages
+
+setup (
+    name = "django-jquery-file-upload",
+    version = "0.0.1",
+    url = "",
+    license = "The MIT License (MIT)",
+    description = "Music Genre Classification",
+    author = 'Sebastian Tschan / Sigurd Gartmann / Indrajith Indraprastham',
+    packages = find_packages(),
+    package_dir = {'': '.'},
+    install_requires = [],
+)
